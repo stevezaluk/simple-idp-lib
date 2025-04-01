03a9f3b9424d97b809de7ee3880b819366e4cde8
Steven A. Zaluk <stevezaluk@gmail.com>
2025-03-31 21:51:41 -0400

    Initial commit for user.go

---
+++ user/user.go
@@ -0,0 +1,14 @@
+package user
+
+/*
+User - A structure representing a user accessing an API
+*/
+type User struct {
+}
+
+/*
+NewUser - A constructor for the User structure
+*/
+func NewUser() *User {
+	return &User{}
+}
