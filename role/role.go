45f915129c36d6d41835bee6f9fe444583e6d6cf
Steven A. Zaluk <stevezaluk@gmail.com>
2025-03-31 21:50:08 -0400

    Added boiler plate for Role structure

--- role/role.go
+++ role/role.go
@@ -1,1 +1,15 @@
 package role
+
+/*
+Role - A user defined role that be attached to a User. Provides a structure
+to attach permissions to
+*/
+type Role struct {
+}
+
+/*
+NewRole -
+*/
+func NewRole() *Role {
+	return &Role{}
+}
