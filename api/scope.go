8ad8e8c826b3ab1435fdd544ed846a01cba6d387
Steven A. Zaluk <stevezaluk@gmail.com>
2025-03-31 21:32:11 -0400

    Outlined a very basic scope structure with a constructor. Defines two fields: identifier and description

--- api/scope.go
+++ api/scope.go
@@ -1,3 +1,22 @@
 package api
 
+/*
+Scope - A user defined permission that can restrict access to resources. Attached at the API level
+*/
+type Scope struct {
+	// Identifier - The identifier of the permission scope. Should follow the following format: action:resource
+	Identifier string `json:"identifier"`
 
+	// Description - A description for what the scope does
+	Description string `json:"description"`
+}
+
+/*
+NewScope - A constructor for the Scope object
+*/
+func NewScope(identifier string, description string) *Scope {
+	return &Scope{
+		Identifier:  identifier,
+		Description: description,
+	}
+}
