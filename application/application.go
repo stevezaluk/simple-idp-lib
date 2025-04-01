b1099d8dfd9f154a8cc8886f4420de8d45a4b298
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-01 19:14:26 -0400

    Updated application structure to generate UUID's on construction

--- application/application.go
+++ application/application.go
@@ -1,15 +1,29 @@
 package application
 
+import "github.com/stevezaluk/simple-idp-lib/uuid"
+
 /*
 Application - A user defined application. Users will define these and authorize there applications
 to use API's that are defined
 */
 type Application struct {
+	// Id - A UUID acting as a unique identifier for the Application
+	Id string `json:"id"`
+
+	// Name - The name of the Application
+	Name string `json:"name"`
 }
 
 /*
 NewApplication - A constructor for the Application struct
 */
-func NewApplication() *Application {
-	return &Application{}
+func NewApplication(name string) (*Application, error) {
+	identifier, err := uuid.GenerateUUID()
+	if err != nil {
+		return nil, err
+	}
+	return &Application{
+		Id:   identifier,
+		Name: name,
+	}, nil
 }
