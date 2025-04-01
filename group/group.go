8b21c67ee17a071708ece22dc52c5c5196f2d4bf
Steven A. Zaluk <stevezaluk@gmail.com>
2025-03-31 21:53:13 -0400

    Initial commit for group.go

---
+++ group/group.go
@@ -0,0 +1,14 @@
+package group
+
+/*
+Group - A user defined object that stores groups of users
+*/
+type Group struct {
+}
+
+/*
+NewGroup - A constructor for the Group structure
+*/
+func NewGroup() *Group {
+	return &Group{}
+}
