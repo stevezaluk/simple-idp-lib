ae34ceabefb884953fbb7267999da803be7d1cb8
Steven A. Zaluk <stevezaluk@gmail.com>
2025-03-31 21:25:37 -0400

    Defined a basic structure for a user defined API

---
+++ api/api.go
@@ -0,0 +1,14 @@
+package api
+
+/*
+API - A user defined application that defines how you can issue tokens
+*/
+type API struct {
+}
+
+/*
+NewAPI - A constructor for the API object
+*/
+func NewAPI() *API {
+	return &API{}
+}
