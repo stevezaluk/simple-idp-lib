fa61dbc1b6b1949458b0b37eafa8d2758a7a4b39
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-03 20:48:10 -0400

    Initial commit for Service.go. This is going to provide re-usable code needed for each microservice

--- server/service.go
+++ server/service.go
@@ -1,1 +1,27 @@
 package server
+
+import "github.com/gin-gonic/gin"
+
+/*
+Service - An abstraction of a Micro-Service. Responsible for exposing the underlying
+gin REST API and providing a connection to MongoDB
+*/
+type Service struct {
+	// Name - The name of the micro-service
+	Name string
+
+	// router - The gin router responsible for
+	router *gin.Engine
+
+	// database - The MongoDB database that this Service is connected to
+	database *Database
+}
+
+/*
+NewService - A constructor for the Service object
+*/
+func NewService(name string) *Service {
+	return &Service{
+		Name: name,
+	}
+}
