4d47e7331a6e22f7c4f73ba26aca9882f5cfe145
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-04 21:54:50 -0400

    Defined a special type called HandlerFunc that takes a service as a paramter and returns a gin handler. This ensures that each handler has access to the Service structure for making database calls

--- server/service.go
+++ server/service.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"strconv"
 )
+
+type HandlerFunc func(service *Service) func(c *gin.Context)
 
 /*
 Service - An abstraction of a Micro-Service. Responsible for exposing the underlying
@@ -12,6 +16,9 @@ gin REST API and providing a connection to MongoDB
 type Service struct {
 	// Name - The name of the Service
 	Name string
+
+	// Port - The port to expose the API on. Defaults to 8080
+	Port int
 
 	// router - The gin router responsible for
 	router *gin.Engine
@@ -24,8 +31,13 @@ type Service struct {
 New - A constructor for the Service object
 */
 func New(name string, database *Database) *Service {
+	router := gin.New()
+	router.Use(gin.Recovery())
+
 	return &Service{
 		Name:     name,
+		Port:     8080,
+		router:   router,
 		database: database,
 	}
 }
@@ -39,4 +51,40 @@ func FromConfig() *Service {
 		viper.GetString("name"),
 		NewDatabaseFromConfig(),
 	)
+}
+
+/*
+RegisterEndpoint - Wraps the gin function gin.Engine.Handle and registers a new endpoint with the
+router. The 'handler' parameter should be the logic of your endpoint using the HandlerFunc type
+*/
+func (service *Service) RegisterEndpoint(method string, endpoint string, handler HandlerFunc) {
+	service.router.Handle(method, endpoint, handler(service))
+}
+
+/*
+Run - Start the Service and expose the API to the port defined in Service.Port. Logic
+in here should eventually be deprecated so it can gracefully shutdown. Gin doesn't
+provide a way natively within its framework to gracefully stop accepting connections
+*/
+func (service *Service) Run() error {
+	err := service.router.Run("0.0.0.0:" + strconv.Itoa(service.Port))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/*
+Shutdown - Gracefully disconnects from the database. This will be improved eventually
+as realistically the API should stop accepting new requests before gracefully disconnecting
+from MongoDB
+*/
+func (service *Service) Shutdown() error {
+	err := service.database.Client().Disconnect(context.Background())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
